361449ebf8d71fb2b8bc237ede5cfa6c2a96a29b
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-11-23 23:37:50 +0500

    Signed-off-by: Tayyba23 <10bittfatima@seecs.edu.pk>

--- SQLSource/ExecScripts.bat
+++ SQLSource/ExecScripts.bat
@@ -4,14 +4,14 @@ echo %ERRORLEVEL%
 SET /A a=%ERRORLEVEL%
 SET /A b=0 
 if %a% NEQ %b% (
-EXIT 1
+   sh "not existcommand"
 )
 
 bteq <ScriptY.txt>  ScriptY_Log.txt
 SET /A a=%ERRORLEVEL%
 SET /A b=0 
 if %a% NEQ %b% (
-EXIT 1
+   sh "not exist command"
 )
 
 
