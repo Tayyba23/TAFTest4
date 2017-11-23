389b5a147fff95ab5e11c7ca7b63547033f0175f
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-11-23 21:47:59 +0500

    Signed-off-by: Tayyba23 <10bittfatima@seecs.edu.pk>

--- SQLSource/ExecScripts.bat
+++ SQLSource/ExecScripts.bat
@@ -2,9 +2,10 @@ echo off
 bteq <ScriptX.txt>  ScriptX_Log.txt
 echo %ERRORLEVEL%
 
-if %ERRORLEVEL% > 0 EXIT -1 
+if %ERRORLEVEL% GTR  0 
+( EXIT -1 )
 bteq <ScriptY.txt>  ScriptY_Log.txt
 
-echo %ERROR_LEVEL% > EXIT -1 
+IF %ERROR_LEVEL% GTR 0 ( EXIT -1 )
 
 
