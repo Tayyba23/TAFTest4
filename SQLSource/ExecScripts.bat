bb09d68c4b86dccea4555ac8f59cee88a7e53ad3
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-11-23 20:47:47 +0500

    Signed-off-by: Tayyba23 <10bittfatima@seecs.edu.pk>

--- SQLSource/ExecScripts.bat
+++ SQLSource/ExecScripts.bat
@@ -1,45 +1,10 @@
 echo off
-bteq <createTableBteq.txt>  StageTableCreation_Log.txt
-set error_level=%ERRORLEVEL%
-echo on
-echo %error_level%
-IF %error_level%==0 (
-goto executeSecond
-)
+bteq <ScriptX.txt>  ScriptX_Log.txt
+echo %ERRORLEVEL%
 
-ELSE 
-goto ShowError
-exit	
+bteq <ScriptY.txt>  ScriptY_Log.txt
 
-:executeSecond
-echo "Executing Second Script - Populating "
-echo off
-tbuild -f load_data_in_stage.tpt >loadDataStage_Log.txt
-echo on
-set error_level1=%ERRORLEVEL%
-echo %ERRORLEVEL% > errorlevel_loadStage.txt
-IF %error_level1%==0 (
-goto executeThird
-)
-ELSE
-goto ShowError
-exit 
+echo %ERROR_LEVEL%
 
 
-
-:executeThird
-echo "Executing third script"
-echo off
-bteq <createTargetTableBteq.txt>  targetTableCreation_Log.txt
-set error_level=%ERRORLEVEL% 
-tbuild -f load_data_in_target.tpt > loadDataTarget_Log.txt
-echo on
-echo %ERRORLEVEL% > errorlevel_loadTarget.txt
-goto commonExit
-
-:ShowError
-echo "Error in one of script file. Check Log Files"
-goto commonExit
-
-:commonExit
-echo "Exiting Batch File"
+echo %ERROR_LEVEL%
