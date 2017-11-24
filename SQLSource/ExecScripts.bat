6dadb4b1623334825f27a78a26f9d888c46f33a7
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-11-24 15:23:28 +0500

    Signed-off-by: Tayyba23 <10bittfatima@seecs.edu.pk>

--- SQLSource/ExecScripts.bat
+++ SQLSource/ExecScripts.bat
@@ -4,14 +4,13 @@ echo %ERRORLEVEL%
 SET /A a=%ERRORLEVEL%
 SET /A b=0 
 if %a% NEQ %b% (
-EXIT 1
 )
 
 bteq <ScriptY.txt>  ScriptY_Log.txt
 SET /A a=%ERRORLEVEL%
 SET /A b=0 
 if %a% NEQ %b% (
-  EXIT 1
+  
 )
 
 
