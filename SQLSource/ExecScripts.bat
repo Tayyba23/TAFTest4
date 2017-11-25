2144acf6c5f268d60741245cf7cfa3866583e450
Tayyba23 <10bittfatima@seecs.edu.pk>
2017-11-25 16:48:58 +0500

    Signed-off-by: Tayyba23 <10bittfatima@seecs.edu.pk>

--- SQLSource/ExecScripts.bat
+++ SQLSource/ExecScripts.bat
@@ -2,3 +2,11 @@
 bteq <ScriptX.txt>  ScriptX_Log.txt 2>error_logfile.txt
 echo %ERRORLEVEL%
 
+
+bteq <ScriptY.txt>  ScriptY_Log.txt 2>error_logfile.txt
+echo %ERRORLEVEL%
+
+
+bteq <ScriptZ.txt>  ScriptX_Log.txt 2>error_logfile.txt
+echo %ERRORLEVEL%
+
